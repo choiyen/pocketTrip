8efcda2d6ce19a9def98081983e437e7ed2d520a
syc031023 <syc031023@naver.com>
2025-01-18 12:38:21 +0900

    dto 작업

--- backend/backend/src/main/java/project/backend/DTO/ApplicantsDTO.java
+++ backend/backend/src/main/java/project/backend/DTO/ApplicantsDTO.java
@@ -1,4 +1,13 @@
 package project.backend.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class ApplicantsDTO {
+    private String travelcode;
+    private List<String> userList;
 }
--- backend/backend/src/main/java/project/backend/DTO/UserDTO.java
+++ backend/backend/src/main/java/project/backend/DTO/UserDTO.java
@@ -1,4 +1,14 @@
 package project.backend.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserDTO {
+    private String userid;
+    private String password;
+    private String name;
+    private String email;
+    private String phone;
 }
