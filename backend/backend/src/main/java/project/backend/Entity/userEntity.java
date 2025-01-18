4349465171de3f902a0c44e3828d065d8b1384cd
choiyen <yyy1459@naver.com>
2025-01-18 12:29:42 +0900

    백엔드 파일 빌드 문제 수정

--- backend/backend/src/main/java/project/backend/Entity/userEntity.java
+++ backend/backend/src/main/java/project/backend/Entity/userEntity.java
@@ -1,4 +1,24 @@
 package project.backend.Entity;
 
-public class userEntity {
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Entity
+@Document(collection = "users")
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class userEntity
+{
+    @Id
+    private String id;
+    private String userid;
+    private String password;
+    private String name;
+    private String email;
+    private String phone;
 }
