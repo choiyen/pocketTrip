2ba0cd1dc287a382ccdc0583c034e827b196e59b
syc031023 <syc031023@naver.com>
2025-01-21 10:44:02 +0900

    user 정보 수정

--- backend/backend/src/main/java/project/backend/Entity/ExpenditureEntity.java
+++ backend/backend/src/main/java/project/backend/Entity/ExpenditureEntity.java
@@ -5,7 +5,7 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Document(collection = "expenditures")
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -16,14 +16,29 @@ public class ExpenditureEntity
 {
     @Id
     private String id;
+    private String travelCode;
     private String purpose;
-    private String type;
     private String method;
     private boolean isPublic;
     private String payer;
-    private LocalDateTime date;
+    private LocalDate date;
     private int KRW;
     private int amount;
     private String currency;
     private String description;
+
+    public static ExpenditureEntity of(String purpose, String travelCode, String method, boolean isPublic, String payer, LocalDate date, int KRW, int amount, String currency, String description) {
+        return ExpenditureEntity.builder()
+                .purpose(purpose)
+                .travelCode(travelCode)
+                .method(method)
+                .isPublic(isPublic)
+                .payer(payer)
+                .date(date)
+                .KRW(KRW)
+                .amount(amount)
+                .currency(currency)
+                .description(description)
+                .build();
+    }
 }
