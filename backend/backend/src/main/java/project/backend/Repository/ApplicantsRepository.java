5bbb54fba9c707ef29426f049707492966713d55
choiyen <yyy1459@naver.com>
2025-01-21 11:04:59 +0900

    travelplan CLUD 완료

--- backend/backend/src/main/java/project/backend/Repository/ApplicantsRepository.java
+++ backend/backend/src/main/java/project/backend/Repository/ApplicantsRepository.java
@@ -5,9 +5,17 @@ import org.springframework.stereotype.Repository;
 import project.backend.Entity.ApplicantsEntity;
 import project.backend.Entity.ExpenditureEntity;
 import project.backend.Entity.TravelPlanEntity;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface ApplicantsRepository extends ReactiveMongoRepository<ApplicantsEntity, String>
 {
-    ApplicantsEntity findByTravelCode(String travelCode);
+    Mono<ApplicantsEntity> findByTravelCode(String travelCode);
+
+    // 주어진 여행코드를 가진 여행 신청자 문서가 존재하는지 여부 확인
+    Boolean existsByTravelCode(String travelCode);
+
+    Mono<Void> deleteByTravelCode(String travelCode);
+
+
 }
