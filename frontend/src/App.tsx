1c3d6573bbcd4f17fad62a47ab0b45acff7515f5
kimminjeong2 <zbdj0123@naver.com>
2025-01-22 10:16:02 +0900

    로그인_디자인끝

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import "./styles/reset.css";
 import "./styles/global.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MainPage from "./components/pages/MainPage";
-import MyPage from "./components/pages/MyPage";
+import MainPage from "./pages/Home/MainPage";
+import MyPage from "./pages/Mypage/MyPage";
+import Login from "./pages/Login/Login";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -13,6 +14,7 @@ function App() {
       <Provider store={store}>
         <BrowserRouter>
           <Routes>
+            <Route path="/Login" element={<Login />} />
             <Route path="/" element={<MainPage />} />
             <Route path="/mypage" element={<MyPage />} />
           </Routes>
