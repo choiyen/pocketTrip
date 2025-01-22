3bab2da033d6fc33cd508d7483aa5116a6bdc9fa
HyunWeb <mattew1803@naver.com>
2025-01-22 09:13:00 +0900

    컴포넌트 정리방식 페이지 기반 분류로 변경

--- frontend/src/pages/Mypage/MyPage.tsx
+++ frontend/src/pages/Mypage/MyPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import Header from "../templates/Header";
+import Header from "../../components/Common/Header";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store";
 import { ChangeCurrentPage } from "../../slices/currentPageSlice";
