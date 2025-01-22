db526f6bf99c7b499a0f61aaeff92249365e6269
syc031023 <syc031023@naver.com>
2025-01-22 13:54:13 +0900

    expenditure 조회 / 수정

--- backend/backend/src/main/java/project/backend/Controller/ExpendituresController.java
+++ backend/backend/src/main/java/project/backend/Controller/ExpendituresController.java
@@ -17,6 +17,7 @@ import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Random;
 
 /*
 MongoDB에서는 컬렉션이 데이터가 실제로 삽입될 때 자동으로 생성됩니다.
@@ -30,15 +31,34 @@ public class ExpendituresController {
     private ExpenditureService expenditureService;
 
     // 지출 추가
-    @PostMapping
-    public ResponseEntity<?> createExpenditure(@AuthenticationPrincipal String userId, @RequestBody ExpendituresDTO expendituresDTO){
+    @PostMapping("/{travelCode}")
+    public ResponseEntity<?> createExpenditure(@AuthenticationPrincipal String userId, @RequestBody ExpendituresDTO expendituresDTO, @PathVariable String travelCode) {
         try {
+            int leftLimit = 48; // numeral '0'
+            int rightLimit = 122; // letter 'z'
+            int targetStringLength = 8;
+            Random random = new Random();
+            String generatedString;
+            // 16 바이트 키 (AES-128)
+
+            while (true) {
+                generatedString = random.ints(leftLimit, rightLimit + 1)
+                        .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                        .limit(targetStringLength)
+                        .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                        .toString();
+                if (expenditureService.SelectExpenditureId(generatedString) == false) {
+                    break;
+                }
+            }
+
             ExpenditureEntity expenditure = ExpenditureEntity.builder()
-                    .travelCode(expendituresDTO.getTravelCode())
+                    .travelCode(travelCode)
+                    .expenditureId(generatedString)
                     .purpose(expendituresDTO.getPurpose())
                     .method(expendituresDTO.getMethod())
                     .isPublic(expendituresDTO.isPublic())
-                    .payer(userId)
+                    .payer(expendituresDTO.getPayer())
                     .date(expendituresDTO.getDate())
                     .KRW(expendituresDTO.getKRW())
                     .amount(expendituresDTO.getAmount())
@@ -49,6 +69,7 @@ public class ExpendituresController {
             ExpenditureEntity createExpenditure = expenditureService.create(expenditure);
             ExpendituresDTO responsedDTO = ExpendituresDTO.builder()
                     .travelCode(createExpenditure.getTravelCode())
+                    .expenditureId(createExpenditure.getExpenditureId())
                     .purpose(createExpenditure.getPurpose())
                     .method(createExpenditure.getMethod())
                     .isPublic(createExpenditure.isPublic())
@@ -78,12 +99,50 @@ public class ExpendituresController {
 
 
     // 지출 수정
+    @PutMapping("/{travelCode}/{expenditureId}")
+    public ResponseEntity<?> updateExpenditure(@AuthenticationPrincipal String userId, @RequestBody ExpendituresDTO expendituresDTO, @PathVariable String travelCode, @PathVariable String expenditureId) {
+        try {
+            ExpenditureEntity expenditure = ExpenditureEntity.builder()
+                    .travelCode(travelCode)
+                    .expenditureId(expenditureId)
+                    .purpose(expendituresDTO.getPurpose())
+                    .method(expendituresDTO.getMethod())
+                    .isPublic(expendituresDTO.isPublic())
+                    .payer(expendituresDTO.getPayer())
+                    .date(expendituresDTO.getDate())
+                    .KRW(expendituresDTO.getKRW())
+                    .amount(expendituresDTO.getAmount())
+                    .currency(expendituresDTO.getCurrency())
+                    .description(expendituresDTO.getDescription())
+                    .build();
+
+            ExpenditureEntity updateExpenditure = expenditureService.updateExpenditure(expenditureId, expenditure);
+
+            ExpendituresDTO responsedDTO = ExpendituresDTO.builder()
+                    .id(updateExpenditure.getId())
+                    .travelCode(updateExpenditure.getTravelCode())
+                    .expenditureId(updateExpenditure.getExpenditureId())
+                    .purpose(updateExpenditure.getPurpose())
+                    .method(updateExpenditure.getMethod())
+                    .isPublic(updateExpenditure.isPublic())
+                    .payer(updateExpenditure.getPayer())
+                    .date(updateExpenditure.getDate())
+                    .KRW(updateExpenditure.getKRW())
+                    .amount(updateExpenditure.getAmount())
+                    .currency(updateExpenditure.getCurrency())
+                    .description(updateExpenditure.getDescription())
+                    .build();
+
+            return ResponseEntity.ok().body(responsedDTO);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
     // 지출 삭제
 
-}
-
-
 
 //마지막 과제 : 카드 별, 현금 별로 데이터 뽑아오는 함수 추가
+}
--- backend/backend/src/main/java/project/backend/Service/ExpenditureService.java
+++ backend/backend/src/main/java/project/backend/Service/ExpenditureService.java
@@ -5,16 +5,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import project.backend.Entity.ExpenditureEntity;
 import project.backend.Entity.TravelPlanEntity;
+import project.backend.Entity.UserEntity;
 import project.backend.Repository.ExpendituresRepository;
 import project.backend.Repository.TravelPlanRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 import java.util.List;
 
 @Slf4j
 @Service
 public class ExpenditureService {
+
+    private final String key = "1234567890123456";
 
     @Autowired
     private ExpendituresRepository expendituresRepository;
@@ -29,6 +35,44 @@ public class ExpenditureService {
 
     public Flux<ExpenditureEntity> findAllByTravelCode(String travelCode) {
         return expendituresRepository.findAllByTravelCode(travelCode);
+    }
+
+    public boolean SelectExpenditureId(String expenditureId) throws Exception {
+        Boolean bool = expendituresRepository.existsByExpenditureId(encrypt(expenditureId ,key)).block();
+        return  bool;
+    }
+
+    //암호화 코드 작성
+    public static String encrypt(String data, String key) throws Exception
+    {
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedData = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedData);
+    }
+
+    public ExpenditureEntity updateExpenditure(String expenditureId, ExpenditureEntity expenditureEntity) {
+        ExpenditureEntity originalEntity = expendituresRepository.findByExpenditureId(expenditureId).block();
+
+        if(originalEntity == null) {
+            log.warn("Expenditure with id {} does not exist", expenditureId);
+        }
+
+        originalEntity.setPurpose(expenditureEntity.getPurpose());
+        originalEntity.setMethod(expenditureEntity.getMethod());
+        originalEntity.setPublic(expenditureEntity.isPublic());
+        originalEntity.setPayer(expenditureEntity.getPayer());
+        originalEntity.setDate(expenditureEntity.getDate());
+        originalEntity.setKRW(expenditureEntity.getKRW());
+        originalEntity.setAmount(expenditureEntity.getAmount());
+        originalEntity.setCurrency(expenditureEntity.getCurrency());
+        originalEntity.setDescription(expenditureEntity.getDescription());
+
+
+        ExpenditureEntity updatedExpenditure = expendituresRepository.save(originalEntity).block();
+
+        return updatedExpenditure;
     }
 
     private Mono<Void> validate(ExpenditureEntity expenditureEntity) {
