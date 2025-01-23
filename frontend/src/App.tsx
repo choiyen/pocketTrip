1f2910922293dbd047bd29a3faffabc4748c44c4
kimminjeong2 <zbdj0123@naver.com>
2025-01-23 10:32:41 +0900

    로그인,회원가입css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/Home/MainPage";
 import MyPage from "./pages/Mypage/MyPage";
 import Login from "./pages/Login/Login";
-import Register from "./pages/Login/Register/Register";
+import Register from "./pages/Register/Register";
 import { Provider } from "react-redux";
 import store from "./store";
 
