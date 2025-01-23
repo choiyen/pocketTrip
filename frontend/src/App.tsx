c00c2d6a60be0702bfcec6aac5366763a44a9fff
kimminjeong2 <zbdj0123@naver.com>
2025-01-23 13:59:43 +0900

    지갑만들기

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,10 @@ import MainPage from "./pages/Home/MainPage";
 import MyPage from "./pages/Mypage/MyPage";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
-import Where from "./pages/Where/Where";
+import Where1 from "./pages/Where/Where1";
+import Where2 from "./pages/Where/Where2";
+import Where3 from "./pages/Where/Where3";
+import Where4 from "./pages/Where/Where4";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -20,7 +23,10 @@ function App() {
             <Route path="/Login/Register" element={<Register />} />
             <Route path="/" element={<MainPage />} />
             <Route path="/mypage" element={<MyPage />} />
-            <Route path="/Where" element={<Where />} />
+            <Route path="/Where1" element={<Where1 />} />
+            <Route path="/Where2" element={<Where2 />} />
+            <Route path="/Where3" element={<Where3 />} />
+            <Route path="/Where4" element={<Where4 />} />
           </Routes>
         </BrowserRouter>
       </Provider>
