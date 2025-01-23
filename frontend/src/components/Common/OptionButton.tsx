f6d8c095844493f6bed686a49686673b166fdb69
HyunWeb <mattew1803@naver.com>
2025-01-23 11:07:46 +0900

    버튼 제작완료, 옵션 메뉴 제작 중

---
+++ frontend/src/components/Common/OptionButton.tsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import { SlOptionsVertical } from "react-icons/sl";
+import { HiMiniPencilSquare } from "react-icons/hi2";
+import { FaTrash } from "react-icons/fa";
+import styled from "styled-components";
+import { AppDispatch, RootState } from "../../store";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  ChangeModalState,
+  ChangeMovingModal,
+} from "../../slices/ModalControlSlice";
+
+const OptionWrap = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+`;
+const Button = styled.button`
+  width: 40px;
+  height: 40px;
+  font-size: 18px;
+  background-color: transparent;
+  border: none;
+`;
+const OptionMenu = styled.ul`
+  background-color: white;
+  position: absolute;
+  top: 100%;
+  right: 100%;
+  width: max-content;
+  border-radius: 10px;
+`;
+const MenuButton = styled.button`
+  width: 123px;
+  font-size: 15px;
+  font-family: inherit;
+  white-space: nowrap;
+  text-align: left;
+  padding: 10px;
+  line-height: 1.2;
+  display: flex;
+  align-items: center;
+  background-color: transparent;
+  border: none;
+  border-radius: 10px;
+  color: #5c5c5c;
+  &:hover {
+    background-color: #e0e0e0;
+  }
+  svg {
+    font-size: 18px;
+    margin-right: 5px;
+  }
+`;
+
+const DeleteButton = styled(MenuButton)`
+  color: #cc0003;
+`;
+
+export default function OptionButton() {
+  const [visibleOption, setVisibleOption] = useState(false);
+
+  const dispatch: AppDispatch = useDispatch();
+  const modalState = useSelector(
+    (state: RootState) => state.modalControl.modalState
+  );
+
+  // 버튼 동작에 따라서 모달창이 on/off된다.
+  const ChangeState = () => {
+    // 모달창이 렌더링 되기 전이면 렌더링 후 등장
+    if (modalState === false) {
+      dispatch(ChangeModalState());
+      setTimeout(() => {
+        dispatch(ChangeMovingModal());
+      }, 50);
+    } else {
+      // // 모달창이 렌더링 되어 있는 상태면 내리는 동작 이후 제거
+      dispatch(ChangeMovingModal());
+      setTimeout(() => {
+        dispatch(ChangeModalState());
+      }, 500);
+    }
+    setVisibleOption(false);
+  };
+
+  return (
+    <OptionWrap>
+      <Button onClick={() => setVisibleOption((prev) => !prev)}>
+        <SlOptionsVertical />
+      </Button>
+      {visibleOption && (
+        <OptionMenu>
+          <li>
+            <MenuButton onClick={() => ChangeState()}>
+              <HiMiniPencilSquare />내 정보 수정
+            </MenuButton>
+          </li>
+          <li>
+            <DeleteButton>
+              <FaTrash />
+              삭제
+            </DeleteButton>
+          </li>
+        </OptionMenu>
+      )}
+    </OptionWrap>
+  );
+}
