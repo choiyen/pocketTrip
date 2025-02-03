5d9f39b0884b671dafd4d014041d09ff86083ccd
choiyen <yyy1459@naver.com>
2025-02-03 14:01:21 +0900

    MongoDB entity에 @Nonnull

---
+++ backend/backend/src/main/java/project/backend/Socket/WebSocketChatHandler.java
@@ -0,0 +1,35 @@
+package project.backend.Socket;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+
+import static org.springframework.messaging.simp.stomp.StompHeaders.SESSION;
+
+public class WebSocketChatHandler implements HandshakeInterceptor
+{
+
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        if(request instanceof ServletServerHttpRequest)
+        {
+            ServletServerHttpRequest servletServerHttpRequest = (ServletServerHttpRequest) request;
+            HttpSession session = servletServerHttpRequest.getServletRequest().getSession();
+            attributes.put(SESSION,session);
+        }
+
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+    }
+    //https://hyeooona825.tistory.com/89
+    //웹소켓 관련 설명
+}
