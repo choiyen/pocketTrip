f112fdbd235223609965f29d609ee595cb2b274c
syc031023 <syc031023@naver.com>
2025-02-04 14:03:38 +0900

    userTravels 테이블 추가 작업

---
+++ backend/backend/src/main/java/project/backend/Entity/UserTravelsEntity.java
@@ -0,0 +1,21 @@
+package project.backend.Entity;
+
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+
+@Document(collection = "userTravels")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserTravelsEntity {
+    @Id
+    private String id;
+    @NonNull
+    private String email;
+    private ArrayList<String> travelList;
+}
