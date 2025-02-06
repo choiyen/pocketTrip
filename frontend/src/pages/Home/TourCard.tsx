88f644d3e6c4ceab396f86b8824267851ac6b0a8
HyunWeb <mattew1803@naver.com>
2025-02-06 13:33:31 +0900

    메인페이지 제작 완료

--- frontend/src/pages/Home/TourCard.tsx
+++ frontend/src/pages/Home/TourCard.tsx
@@ -36,6 +36,8 @@ const Card = styled(Link)<{ $bgImg: string }>`
   justify-content: flex-end;
   color: white;
   padding: 15px;
+  box-sizing: border-box;
+  box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.09);
 
   h2 {
     font-size: 32px;
--- frontend/src/pages/Mypage/MyPage.tsx
+++ frontend/src/pages/Mypage/MyPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import Header from "../../components/Common/Header";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "../../store";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../store";
 import { ChangeCurrentPage } from "../../slices/currentPageSlice";
 import styled from "styled-components";
 import OptionButton from "../../components/Common/OptionButton";
@@ -10,10 +10,10 @@ import { useNavigate } from "react-router-dom";
 export default function MyPage() {
   const dispatch: AppDispatch = useDispatch();
   const navigate = useNavigate();
+
   useEffect(() => {
     dispatch(ChangeCurrentPage("mypage"));
   }, []);
-
 
   const travelList: any[] = [
     {
@@ -36,41 +36,58 @@ export default function MyPage() {
       startDate: "25.01.27",
       endDate: "25.02.02",
       expense: 2000000,
-    }
-  ]
+    },
+  ];
 
   return (
     <div>
       <Header />
-      <div style={{overflow: "scroll"}}>
       <ProfileContainer>
         {/* <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
           <circle cx="10.5" cy="3.5" r="1.5" fill="#1C1C1C" />
           <circle cx="10.5" cy="10.5" r="1.5" fill="#1C1C1C" />
           <circle cx="10.5" cy="17.5" r="1.5" fill="#1C1C1C" />
         </svg> */}
-        <OptionButton className="profileButton"/>
+        <OptionButton className="profileButton" />
         <Profile>
-          <img src="" alt="" />
+          <img src="/profileImage.png" alt="" />
           <span>name</span>
         </Profile>
       </ProfileContainer>
-      {travelList.length != 0 ?
+      {travelList.length != 0 ? (
         <TravelListContainer>
           <TravelList>
             {travelList.map((travel) => {
               return (
                 <Travel id={travel.travelCode}>
-                  <div style={{ padding: "20px 15px", display: "flex", flexDirection: "column", height: "100%", justifyContent: "space-between" }}>
+                  <div
+                    style={{
+                      padding: "20px",
+                      display: "flex",
+                      flexDirection: "column",
+                      height: "100%",
+                      justifyContent: "space-around",
+                    }}
+                  >
                     <Title>{travel.title}</Title>
                     <Expense>{travel.expense}</Expense>
                   </div>
 
-                  <div style={{ padding: "20px 0", display: "flex", flexDirection: "column", height: "100%", justifyContent: "space-between" }}>
-                    <Duration>{travel.startDate} - {travel.endDate}</Duration>
+                  <div
+                    style={{
+                      padding: "20px",
+                      display: "flex",
+                      flexDirection: "column",
+                      height: "100%",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <Duration>
+                      {travel.startDate} - {travel.endDate}
+                    </Duration>
                     {/* 참여자 목록 추가 */}
                   </div>
-                  <OptionButton className="travelButton"/>
+                  <OptionButton className="travelButton" />
                 </Travel>
               );
             })}
@@ -79,21 +96,30 @@ export default function MyPage() {
               <div>+</div>
             </AddTravel>
           </TravelList>
-
         </TravelListContainer>
-        :
+      ) : (
         <NoTravelList>
-          {/* <svg width="172" height="268" viewBox="0 0 172 268" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M58.1718 178.562C58.1718 147.384 66.6618 130.063 89.6161 112.743C108.168 98.5711 118.23 89.1234 118.23 74.0071C118.23 58.2609 106.91 48.4982 87.1005 48.4982C66.0329 48.4982 54.713 63.2997 54.713 79.3608H0C0 34.0117 35.8464 0 86.7861 0C141.185 0 172 31.1775 172 74.0071C172 97.3114 160.68 115.892 140.241 131.638C117.287 149.589 110.055 160.926 110.055 178.562H58.1718ZM85.8428 268C66.6618 268 52.5119 254.143 52.5119 234.933C52.5119 215.723 66.6618 201.551 85.8428 201.551C105.024 201.551 119.174 215.723 119.174 234.933C119.174 254.143 105.024 268 85.8428 268Z" fill="#E8E8E8" fill-opacity="0.45" />
-          </svg> */}
+          <svg
+            width="172"
+            height="268"
+            viewBox="0 0 172 268"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M58.1718 178.562C58.1718 147.384 66.6618 130.063 89.6161 112.743C108.168 98.5711 118.23 89.1234 118.23 74.0071C118.23 58.2609 106.91 48.4982 87.1005 48.4982C66.0329 48.4982 54.713 63.2997 54.713 79.3608H0C0 34.0117 35.8464 0 86.7861 0C141.185 0 172 31.1775 172 74.0071C172 97.3114 160.68 115.892 140.241 131.638C117.287 149.589 110.055 160.926 110.055 178.562H58.1718ZM85.8428 268C66.6618 268 52.5119 254.143 52.5119 234.933C52.5119 215.723 66.6618 201.551 85.8428 201.551C105.024 201.551 119.174 215.723 119.174 234.933C119.174 254.143 105.024 268 85.8428 268Z"
+              fill="#E8E8E8"
+              fill-opacity="0.45"
+            />
+          </svg>
           <div>
             <p>등록된 여행이 없습니다.</p>
             <span>아직 떠날 준비가 안 되셨나요?</span>
             <span>새로운 여행을 추가해보세요!</span>
             <button onClick={() => navigate("/where1")}>여행 추가</button>
           </div>
-        </NoTravelList>}
-        </div>
+        </NoTravelList>
+      )}
     </div>
   );
 }
@@ -102,7 +128,6 @@ const ProfileContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-
   position: relative;
 
   & .profileButton {
@@ -113,18 +138,16 @@ const ProfileContainer = styled.div`
   }
 `;
 
-
 const Profile = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 20px 0 0 0;
 
-display: flex;
-flex-direction: column;
-// margin: 20px 0 0 0;
-  
-& img {
-  border-radius: 100%;
-  width: 150px;
-  height: 150px;
-  background-color: #111111;}
+  & img {
+    border-radius: 100%;
+    width: 160px;
+    height: 160px;
+  }
 
   & span {
     text-align: center;
@@ -135,36 +158,34 @@ flex-direction: column;
 
 const TravelListContainer = styled.div`
   width: 100%;
-  height: 55vh;
+  height: 60vh;
   // background: black;
   overflow: scroll;
 `;
 
 const NoTravelList = styled.div`
-
   display: flex;
   justify-content: center;
   position: relative;
 
-  & :: before {
-    content: "?";
+  & svg {
     position: absolute;
     top: 140px;
     z-index: -1;
   }
-  
+
   & div {
-  position: relative;
-  z-index: 1;
-  background-color: #EAF6FF;
-  width: 85vw;
-  height: 500px;
-  margin: 20px;
-  border-radius: 15px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+    position: relative;
+    z-index: 1;
+    background-color: #eaf6ff;
+    width: 85vw;
+    height: 500px;
+    margin: 20px;
+    border-radius: 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 
   & p {
@@ -172,7 +193,7 @@ const NoTravelList = styled.div`
     font-weight: 700;
     margin: 20px;
   }
-  
+
   & span {
     font-size: 16px;
     font-weight: 500;
@@ -181,7 +202,7 @@ const NoTravelList = styled.div`
   }
 
   & button {
-    background-color: #0077CC;
+    background-color: #0077cc;
     color: white;
     width: 110px;
     height: 35px;
@@ -195,14 +216,13 @@ const NoTravelList = styled.div`
 const TravelList = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
 `;
 
 const Travel = styled.div`
   width: 85vw;
   height: 100px;
-  background-color: #005EFF;
+  background-color: #0085e5;
   border-radius: 15px;
   margin: 10px 0;
   // position: relative;
@@ -216,11 +236,11 @@ const Travel = styled.div`
     top: 0;
     right: 0;
     margin: 10px 0;
-    
+
     & button svg {
       fill: white;
     }
-    
+
     & ul li button svg {
       fill: currentColor;
     }
@@ -230,15 +250,14 @@ const Travel = styled.div`
 const AddTravel = styled.div`
   width: 85vw;
   height: 100px;
-  background-color: #DFDFDF;
+  background-color: #dfdfdf;
   border-radius: 15px;
-  margin: 15px 0;
   display: flex;
   justify-content: center;
   align-items: center;
 
   & div {
-    background-color: #6E6E6E66;
+    background-color: #6e6e6e66;
     border-radius: 100%;
     width: 28px;
     height: 28px;
