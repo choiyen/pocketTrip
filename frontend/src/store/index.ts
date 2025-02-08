ebc9220caa3500599a429869148c4c52302a96a3
HyunWeb <mattew1803@naver.com>
2025-02-08 11:20:26 +0900

    수정 버튼 리듀서 제작 및 연동

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -5,6 +5,7 @@ import ModalControlReducer from "../slices/ModalControlSlice";
 import AlertControlReducer from "../slices/AlertControlSlice";
 import travelReducer from "../slices/travelSlice";
 import RoutePathReducer from "../slices/RoutePathSlice";
+import editReducer from "../slices/editSlice";
 
 const store = configureStore({
   reducer: {
@@ -14,6 +15,7 @@ const store = configureStore({
     AlertControl: AlertControlReducer,
     travel: travelReducer,
     prevPath: RoutePathReducer,
+    edit: editReducer,
   },
 });
 
