90e7c767bfbb3a96bcea8cb6a0e324470f75621a
choiyen <yyy1459@naver.com>
2025-02-10 11:31:30 +0900

    token 날짜 만료 시 토큰 필터에서 에러 코드를 반환하도록 수정함 모든 기능 동작 확인 후 토큰 만료 후의 상태를 확인할 필요가 있어보임

--- backend/backend/src/main/java/project/backend/Security/JwtAuthenticationFilter.java
+++ backend/backend/src/main/java/project/backend/Security/JwtAuthenticationFilter.java
@@ -17,6 +17,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.rmi.server.ExportException;
 
 
 @Slf4j
@@ -34,10 +35,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             log.info("JwtAuthenticationFilter is running...");
 
             // token 검사
-            if(token != null && !token.equalsIgnoreCase("null"))
+            if (token != null && !token.equalsIgnoreCase("null"))
             {
                 String userId = tokenProvider.validateAndGetUserId(token);
                 log.info("Authenticated userId: {}", userId);
+
+                if (!tokenProvider.validateToken(token))
+                {
+                    //토큰의 날짜를 확인하여, 현재 날짜 기준 만료 날짜가 지난 토큰일 경우 에러 코드를 생성함.
+                    throw new ExportException("토큰 만료로 인해 DB에 접속할 수 없습니다.");
+                }
 
                 AbstractAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userId, null, AuthorityUtils.NO_AUTHORITIES);
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
@@ -46,6 +53,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 securityContext.setAuthentication(authentication);
                 SecurityContextHolder.setContext(securityContext);
             }
+        }
+        catch (ExportException e)
+        {
+            log.error(e.getMessage());
         }
         catch (Exception e)
         {
