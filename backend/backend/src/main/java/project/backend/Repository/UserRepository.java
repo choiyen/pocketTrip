37ccd38d42df4bcd4769d89885377b399588f628
choiyen <yyy1459@naver.com>
2025-02-12 10:10:56 +0900

    user 정보 불러오는 코드 추가

--- backend/backend/src/main/java/project/backend/Repository/UserRepository.java
+++ backend/backend/src/main/java/project/backend/Repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import project.backend.Entity.UserEntity;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository("mongoUserRepository")  // MongoDB 용 빈 이름 명시적으로 설정
@@ -15,4 +16,7 @@ public interface UserRepository extends MongoRepository<UserEntity, String> {
 
     // 주어진 아이디로 사용자 찾아 반환
     UserEntity findByEmail(String userid);
+
+    List<String> findProfileUrlByEmailIn(List<String> emails);
+
 }
