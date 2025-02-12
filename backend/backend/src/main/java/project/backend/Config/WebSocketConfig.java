44470b047d7dea2964ee6f167620cc954990b8e4
choiyen <yyy1459@naver.com>
2025-02-12 12:14:06 +0900

    백엔드 소켓 수정 JWT 인증으로 변경

--- backend/backend/src/main/java/project/backend/Config/WebSocketConfig.java
+++ backend/backend/src/main/java/project/backend/Config/WebSocketConfig.java
@@ -1,15 +1,21 @@
 package project.backend.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+import project.backend.Socket.HttpHandshakeInterceptor;
 
 @Configuration
-@EnableWebSocketMessageBroker
+@EnableWebSocket
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Autowired
+    private HttpHandshakeInterceptor handshakeInterceptor;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
