f700f24a049b49b21db409624f01d1fe72695143
HyunWeb <mattew1803@naver.com>
2025-02-12 12:07:25 +0900

    소켓 방식 변경

--- backend/backend/src/main/java/project/backend/Config/WebSocketConfig.java
+++ backend/backend/src/main/java/project/backend/Config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/travelPlan").addInterceptors(new HttpSessionHandshakeInterceptor()).withSockJS();
+        registry.addEndpoint("/ws/travelPlan").setAllowedOriginPatterns("*") .addInterceptors(new HttpSessionHandshakeInterceptor()).withSockJS();
 
     }
 }
