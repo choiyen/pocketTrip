b7f94d1f88631a42575077911cb88861706b68e4
choiyen <yyy1459@naver.com>
2025-02-20 20:27:08 +0900

    eeee

--- backend/backend/src/main/java/project/backend/Config/WebSecurityConfig.java
+++ backend/backend/src/main/java/project/backend/Config/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOriginPatterns(Arrays.asList("*"));  // 특정 도메인 허용
+        config.setAllowedOriginPatterns(Arrays.asList("http://3.35.49.29:3000","http://localhost:3000", "http://localhost:9000"));  // 특정 도메인 허용
         config.setAllowedMethods(Arrays.asList("HEAD", "POST", "GET", "DELETE", "PUT", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));  // 🔹 허용할 헤더 추가
         config.setExposedHeaders(Arrays.asList("Authorization", "Content-Type")); // 🔹 클라이언트에서 접근 가능하도록 노출
