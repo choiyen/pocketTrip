eb617e66d622382fcd4e3b36611cce97025cc35a
HyunWeb <mattew1803@naver.com>
2025-02-21 09:23:52 +0900

    배포 과정

--- backend/backend/src/main/java/project/backend/Config/WebSocketConfig.java
+++ backend/backend/src/main/java/project/backend/Config/WebSocketConfig.java
@@ -27,9 +27,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000")  // 특정 출처만 허용
+                .setAllowedOrigins("http://13.124.212.22:81")  // 특정 출처만 허용
                 .withSockJS(); // SockJS 사용 설정
     }
-
 }
 
