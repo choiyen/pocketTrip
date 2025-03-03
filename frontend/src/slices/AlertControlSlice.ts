b332efdcff6ba74d89cbc5d08bdc153b6ae105a0
HyunWeb <mattew1803@naver.com>
2025-03-03 23:37:08 +0900

    알림창 리덕스 제작중

--- frontend/src/slices/AlertControlSlice.ts
+++ frontend/src/slices/AlertControlSlice.ts
@@ -7,11 +7,15 @@ const initialState: alertControlState = {
   alertState: false,
 };
 
+// 알림 바 컨트롤 글로벌 상태관리
 const AlertControlSlice = createSlice({
   name: "AlertControl",
   initialState,
   reducers: {
     ChangeAlertState(state) {
+      state.alertState = !state.alertState;
+    },
+    ChangeAlertStates(state) {
       state.alertState = !state.alertState;
     },
   },
